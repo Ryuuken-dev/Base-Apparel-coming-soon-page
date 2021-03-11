3cc0729b9bf74c9da6f5312c3c762578a0b3e18b
Ryuuken-dev <FreeRunner17@o2.pl>
2021-03-11 18:16:38 +0100

    JS code update

--- main.js
+++ main.js
@@ -5,30 +5,31 @@ const errorText = document.querySelector('.errorBox__errorMessage');
 
 
 const showError = (message) => {
-    error.style.display="flex";
+    error.style.display = "flex";
     errorText.textContent = message;
 };
 
-dataInput.addEventListener('input', ()=> {
-    if(!(dataInput.value) || dataInput.value) {
+dataInput.addEventListener('input', () => {
+    if (!(dataInput.value) || dataInput.value) {
         error.style.display = "none";
     }
 });
 
-const confirmEmail = function(e) {
+const confirmEmail = function (e) {
     e.preventDefault();
     const userData = dataInput.value;
-    if(userData.includes('@') && (userData) && !(userData.endsWith('@'))) {
-        return
-    } else if(!(userData)) {
+    if (userData.includes('@') && (userData) && !(userData.endsWith('@')) && !(userData.startsWith('@'))) {
+        return;
+    } else if (!(userData)) {
         showError("To pole nie może być puste");
-    } else if(!(userData.includes('@'))) {
+    } else if (!(userData.includes('@'))) {
         showError(`Nazwa musi zawierać znak "@"`);
-    } else {
+    } else if(userData.endsWith('@')) {
         showError(`Adres e-mail jest niepełny, podaj część po znaku "@"`);
+    } else {
+        showError(`Podaj część przed znakiem "@"`); 
     }
-    
+
 }
 
 acceptButton.addEventListener('click', confirmEmail);
-
